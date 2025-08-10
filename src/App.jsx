d4f53245fc421a9f9c3e15f31cd285419e73ddbe
Mo <mo.ghorb@gmail.com>
2025-08-10 20:35:50 +0330

    feat: add react routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import JobCard from './components/JobCard'
 import Home from './pages/Home'
+import {Routes, Route} from 'react-router-dom'
+import Favorites from './pages/Favorites'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <Home />
-    </>
+    <main className='main-content'>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/favorites' element={<Favorites />} />
+      </Routes>
+    </main>
   )
 }
 
---
+++ src/pages/Favorites.jsx
@@ -0,0 +1,10 @@
+function Favorites() {
+    return (
+        <div className="favorites-empty">
+            <h2>No Faorites jobs yet</h2>
+            <p>Add your favorite jobs to your favorites list</p>
+        </div>
+    )
+}
+
+export default Favorites;
