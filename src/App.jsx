e19377fde63a4e3f6d1c66c998aef91a930905b8
Mo <mo.ghorb@gmail.com>
2025-08-10 20:39:38 +0330

    feat: add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,24 @@
 import './App.css'
 import Home from './pages/Home'
-import {Routes, Route} from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Favorites from './pages/Favorites'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
-    <main className='main-content'>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/favorites' element={<Favorites />} />
-      </Routes>
-    </main>
+    <div>
+      <Navbar />
+      <main className='main-content'>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/favorites' element={<Favorites />} />
+        </Routes>
+      </main>
+    </div>
   )
 }
 
-function WelcomeLabel({name}) {
+function WelcomeLabel({ name }) {
   return (
     <div>
       <h1>Hello {name}, Welcome to the app</h1>
