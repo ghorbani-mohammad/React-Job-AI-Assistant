cefa8f1ab20cf0d12cd198138d5471073aca1b44
Mo <mo.ghorb@gmail.com>
2025-08-12 10:35:39 +0330

    feat: enhance Favorites component to display favorite jobs

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,10 +1,25 @@
 import "../css/favorites.css";
+import JobCard from "../components/JobCard";
+import { useFavoriteContext } from "../contexts/Favorites";
 
 function Favorites() {
+    const {favorites} = useFavoriteContext();
+    if (favorites.length === 0) {
+        return (
+            <div className="favorites-empty">
+                <h2>No Faorites jobs yet</h2>
+                <p>Add your favorite jobs to your favorites list</p>
+            </div>
+        )
+    }
     return (
-        <div className="favorites-empty">
-            <h2>No Faorites jobs yet</h2>
-            <p>Add your favorite jobs to your favorites list</p>
+        <div className="favorites">
+            <h2>My Favorites</h2>
+            <div className="favorites-list">
+                {favorites.map((job) => (
+                    <JobCard key={job.id} job={job} />
+                ))}
+            </div>
         </div>
     )
 }
